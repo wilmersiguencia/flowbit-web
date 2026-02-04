4d53d6e3e8a841a48c081a37cb14d0e50304227d
Wilmer Siguencia <wilmeersiguenci@gmail.com>
2026-02-03 19:24:17 -0500

    Sistema multi-idioma i18n configurado

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
+import translations from './translations.js';
+
 const themeThoggle = document.getElementById('theme-toggle');
+const langButton = document.getElementById('lang-switch');
 const body = document.body;
+
+let currentLang = 'es';
+
+langButton.addEventListener('click', () => {
+    //cambiamos el idioma
+    currentLang = currentLang === 'es' ? 'en' : 'es';
+
+    //buscamoas todos los elementos en el atributo data-i18n
+    document.querySelectorAll('[data-i18n}').forEach(element => {
+        const key = element.getAttribute('data-i18n');
+        if (translations[currentLang][key]) {
+            element.textContent = translations[currentLang][key];
+        }
+    });
+
+});
 
 //logica de modo oscuro
 themeThoggle.addEventListener('click', () => {
---
+++ js/translations.js
@@ -0,0 +1,19 @@
+const translations = {
+    "es": {
+       "hero-title": "Automatización sin errores humanos",
+        "hero-subtitle": "Soluciones tecnológicas de alta precisión para startups que buscan escalar sin fricción.",
+        "btn-contact": "Contactar por WhatsApp",
+        "nav-features": "Servicios",
+        "nav-about": "Nosotros" 
+    },
+
+    "en": {
+        "hero-title": "Automation without human error",
+        "hero-subtitle": "High-precision technology solutions for startups looking to scale without friction.",
+        "btn-contact": "Contact via WhatsApp",
+        "nav-features": "Services",
+        "nav-about": "About Us"
+    }
+};
+
+export default translations;
